8404aedc165fa54d6b255fe12f010616f5375a80
george-petrov-2000 <gpetrov2@illinois.edu>
2022-02-18 15:27:20 -0600

    problem3 hw3

--- homeworks/homework3/george/code/code.py
+++ homeworks/homework3/george/code/code.py
@@ -60,18 +60,34 @@ def problem1(filename1,filename2,filename3,filename4,filename5):
     fig3.savefig(filename5); fig3.clear()
 
 # Problem 2 Code        
-def problem2(filename1,filename2,filename3):
+def problem2(filename1,filename2):
     pass
 
 # Problem 3 Code
-def problem3(filename1):
-    pass
+def problem3(filename1,filename2):
+    file1 = pd.read_csv(filename1)
+    
+    # initialize figure properties
+    plt.figure()
+    plt.rcParams.update({"font.family" : "Times", "mathtext.fontset" : "stix", "text.usetex": True})
+    plt.rcParams.update({'font.size': 14})
+    plt.rcParams['axes.axisbelow'] = True
+    plt.rcParams['lines.markersize'] = 5
+    fig = plt.figure(dpi = 500, figsize=[7, 5])
+    plt.xlabel(r"Angle of Attack"); plt.ylabel(r"Coefficient of Moment")
+    
+    #plots
+    plt.plot(file1.iloc[:,0],file1.iloc[:,8],label = 'NACA 0012', color = "dodgerblue", linewidth = 3)
+    
+    # final formatting before saving
+    plt.grid(); plt.legend(); plt.tight_layout()
+    fig.savefig(filename2); fig.clear()
     
     
 # run all problems
 def main():
     problem1('homeworks/homework3/george/data/NACA0012_1.csv','homeworks/homework3/george/data/NACA4412_1.csv','homeworks/homework3/george/plots/problem1_a.png','homeworks/homework3/george/plots/problem1_b.png','homeworks/homework3/george/plots/problem1_c.png')
-    
+    problem3('homeworks/homework3/george/data/NACA0012_3_finite.csv','homeworks/homework3/george/plots/problem3_ai.png')
 if __name__ == "__main__":
     main()
     
