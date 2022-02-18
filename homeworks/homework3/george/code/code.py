35660411dc93be773900ad86e765a286917c9133
george-petrov-2000 <gpetrov2@illinois.edu>
2022-02-18 14:30:06 -0600

    george hw3

--- homeworks/homework3/george/code/code.py
+++ homeworks/homework3/george/code/code.py
@@ -1,44 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-
-def read_csv_file(filename):
-    data = []
-    with open(filename) as f:
+import pandas as pd
         
-        #remove first 9 lines
-        lines = f.readlines()[10:]
-        
-        for index in range(len(lines)):
-            
-            if index == 0:
-                
-                headers = []
-                for i in range(len(lines[index].split(",")[:10])):
-                    attr = lines[index].split(",")[i]
-                    if lines[index+1].split(",")[i] not in lines[index].split(",")[i]:
-                       attr = ' '.join([lines[index].split(",")[i].strip(), lines[index+1].split(",")[i].strip()])
-                    headers.append(attr)
-                
-                continue
-                
-            if index == 1: continue
-            
-            line = {}
-            for i in range(len(headers)):
-                
-                if headers[i] == 'alpha':
-                    line[headers[i]] = int(float(lines[index].split(",")[i]))
-                else:
-                    line[headers[i]] = float(lines[index].split(",")[i])
-            
-            data.append(line)
-    return data
-            
-
 # Problem 1 Code
-def problem1(problem1_data):
-    pass
+def problem1(filename1,filename2,filename3,filename4,filename5):
+    file1 = pd.read_csv(filename1)
+    file2 = pd.read_csv(filename2)
+    
+    # initialize figure properties
+    plt.figure()
+    plt.rcParams.update({"font.family" : "Times", "mathtext.fontset" : "stix", "text.usetex": True})
+    plt.rcParams.update({'font.size': 14})
+    plt.rcParams['axes.axisbelow'] = True
+    plt.rcParams['lines.markersize'] = 5
+    fig = plt.figure(dpi = 500, figsize=[7, 5])
+    plt.xlabel(r"Angle of Attack"); plt.ylabel(r"Coefficient of Lift")
+    
+    #plots
+    plt.plot(file1.iloc[:,0],file1.iloc[:,1],label = 'NACA 0012', color = "dodgerblue", linewidth = 3)
+    plt.plot(file2.iloc[:,0],file2.iloc[:,1],label = 'NACA 4412', color = "red",  linewidth = 3)
+    
+    # final formatting before saving
+    plt.grid(); plt.legend(); plt.tight_layout()
+    fig.savefig(filename3); fig.clear()
+    
+    # initialize figure properties
+    plt.figure()
+    plt.rcParams.update({"font.family" : "Times", "mathtext.fontset" : "stix", "text.usetex": True})
+    plt.rcParams.update({'font.size': 14})
+    plt.rcParams['axes.axisbelow'] = True
+    plt.rcParams['lines.markersize'] = 5
+    fig2 = plt.figure(dpi = 500, figsize=[7, 5])
+    plt.xlabel(r"Angle of Attack"); plt.ylabel(r"Coefficient of Drag")
+    
+    #plots
+    plt.plot(file1.iloc[:,0],file1.iloc[:,2],label = 'NACA 0012', color = "dodgerblue", linewidth = 3)
+    plt.plot(file2.iloc[:,0],file2.iloc[:,2],label = 'NACA 4412', color = "red",  linewidth = 3)
+    
+    # final formatting before saving
+    plt.grid(); plt.legend(); plt.tight_layout()
+    fig2.savefig(filename4); fig2.clear()
+    
+    # initialize figure properties
+    plt.figure()
+    plt.rcParams.update({"font.family" : "Times", "mathtext.fontset" : "stix", "text.usetex": True})
+    plt.rcParams.update({'font.size': 14})
+    plt.rcParams['axes.axisbelow'] = True
+    plt.rcParams['lines.markersize'] = 5
+    fig3 = plt.figure(dpi = 500, figsize=[7, 5])
+    plt.xlabel(r"Coefficient of Lift"); plt.ylabel(r"Coefficient of Drag")
+    
+    #plots
+    plt.plot(file1.iloc[:,1],file1.iloc[:,2],label = 'NACA 0012', color = "dodgerblue", linewidth = 3)
+    plt.plot(file2.iloc[:,1],file2.iloc[:,2],label = 'NACA 4412', color = "red",  linewidth = 3)
+    
+    # final formatting before saving
+    plt.grid(); plt.legend(); plt.tight_layout()
+    fig3.savefig(filename5); fig3.clear()
 
 # Problem 2 Code        
 def problem2(filename1,filename2,filename3):
@@ -51,8 +70,8 @@ def problem3(filename1):
     
 # run all problems
 def main():
-    problem1_data = read_csv_file('homeworks/homework3/george/data/NACA0012.csv')
-    print(problem1_data)
+    problem1('homeworks/homework3/george/data/NACA0012_1.csv','homeworks/homework3/george/data/NACA4412_1.csv','homeworks/homework3/george/plots/problem1_a.png','homeworks/homework3/george/plots/problem1_b.png','homeworks/homework3/george/plots/problem1_c.png')
+    
 if __name__ == "__main__":
     main()
     
